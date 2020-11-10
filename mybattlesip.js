49f0f3e1c71de52d69b60dd5c1d39dad86084291
Diana Rukavishnikova <psihomatika@gmail.com>
2020-11-10 19:10:03 +0300

    added model and view. changed styles input

---
+++ mybattlesip.js
@@ -0,0 +1,29 @@
+const view = {
+    displayMessage: function(msg) {
+    const messageArea = document.getElementById("messageArea");
+    messageArea.innerHTML = msg;
+    },
+    displayHit: function(location) {
+    const cell = document.getElementById(location);
+    cell.setAttribute("class", "hit");
+    },
+    displayMiss: function(location) {
+    const cell = document.getElementById(location);
+    cell.setAttribute("class", "miss");
+    },
+};
+
+var model = {
+    boardSize: 7,
+    numShips: 3,
+    shipsSunk: 0,
+    shipLength: 3,
+    ships: [{ locations: ["06", "16", "26"], hits: ["", "", ""] },
+            { locations: ["24", "34", "44"], hits: ["", "", ""] },
+            { locations: ["10", "11", "12"], hits: ["", "", ""] }],
+    fire: function(guess) {
+        for (var i = 0; i < this.numShips; i++) {
+            var ship = this.ships[i];
+        }
+    }
+};
