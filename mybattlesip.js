bc844929f5c59409bc9baa9fdeb31c399dd15e98
Diana Rukavishnikova <psihomatika@gmail.com>
2020-12-02 13:44:14 +0300

    разработка функции выстрела

--- mybattlesip.js
+++ mybattlesip.js
@@ -23,7 +23,56 @@ var model = {
             { locations: ["10", "11", "12"], hits: ["", "", ""] }],
     fire: function(guess) {
         for (var i = 0; i < this.numShips; i++) {
-            var ship = this.ships[i];
+            let ship = this.ships[i];
+            let index = ship.locations.indexOf(guess);
+            if (index >= 0) {
+                ship.hits[index] = "hit";
+                view.displayHit(guess);
+                view.displayMessage("HIT!");
+                if (this.isSunk(ship)) {
+                    view.displayMessage("You sank my battleship!");
+                    this.shipsSunk++;
+                }
+                return true;
+            }
+        }
+        view.displayMiss(guess);
+        view.displayMessage("You missed.");
+        return false;
+    },
+    isSunk: function(ship) {
+        for (var i = 0; i < this.shipLength; i++) {
+            if (ship.hits[i] !== "hit") {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+function parseGuess(guess) {
+    const alphabet = ["A", "B", "C", "D", "E", "F", "G"];
+    if (guess === null || guess.length !== 2) {
+        alert("Oops, please enter a letter and a number on the board.");
+    } else {
+        firstChar = guess.charAt(0);
+        var row = alphabet.indexOf(firstChar);
+        var column = guess.charAt(1);
+        if (isNaN(row) || isNaN(column)) {
+            alert("Oops, that isn't on the board.");
+        } else if (row < 0 || row >= model.boardSize ||
+            column < 0 || column >= model.boardSize) {
+            alert("Oops, that's off the board!");
+        } else {
+            return row + column;
+        }
+    }
+    return null;
+};
+const controller = {
+    guesses: 0,
+    processGuess: function(guess) {
+        var location = parseGuess(guess);
+        if (location) {
         }
     }
 };
